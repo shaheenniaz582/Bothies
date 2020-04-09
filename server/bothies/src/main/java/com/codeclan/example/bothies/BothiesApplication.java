b735235c1854ead658c51f552e1ddb6b3904572d
Shaheen <shaheenniaz582@gmail.com>
2020-04-10 00:57:13 +0100

    setup

---
+++ server/bothies/src/main/java/com/codeclan/example/bothies/BothiesApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bothies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BothiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BothiesApplication.class, args);
+	}
+
+}
---
+++ server/bothies/src/test/java/com/codeclan/example/bothies/BothiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bothies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BothiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
