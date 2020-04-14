6f6002a694e29470149eb0a1d8100f27ca21b0c7
87johnboyle <john.hazukisan@gmail.com>
2020-04-14 09:59:36 +0100

    Updated review

--- server/bothies/src/main/java/com/codeclan/example/bothies/components/DataLoader.java
+++ server/bothies/src/main/java/com/codeclan/example/bothies/components/DataLoader.java
@@ -2,6 +2,7 @@ package com.codeclan.example.bothies.components;
 
 import com.codeclan.example.bothies.models.Review;
 import com.codeclan.example.bothies.repositories.ReviewRepository;
+import org.apache.tomcat.jni.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,13 +10,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements ApplicationRunner {
+
+
     @Autowired
     ReviewRepository reviewRepository;
 
-    public DataLoader(){
 
-    }
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
 
+
+<<<<<<< HEAD
     public void run(ApplicationArguments args){
         Review review1 = new Review(" .............",5);
         reviewRepository.save(review1);
@@ -25,5 +30,13 @@ public class DataLoader implements ApplicationRunner {
         reviewRepository.save(review3);
         Review review4 = new Review(" .....ok........",2);
         reviewRepository.save(review4);
+=======
+        Review review1 = new Review("good", 5);
+        reviewRepository.save(review1);
+>>>>>>> 30e4b231bc9d0cbc46f67bf6b6f6117ecf81dc6f
     }
 }
+
+
+
+
--- server/bothies/src/main/java/com/codeclan/example/bothies/controllers/ReviewController.java
+++ server/bothies/src/main/java/com/codeclan/example/bothies/controllers/ReviewController.java
@@ -7,27 +7,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value="/reviews")
 public class ReviewController {
 
     @Autowired
     ReviewRepository reviewRepository;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Review>> getReview(@PathVariable Long id){
+    @GetMapping(value = "/reviews")
+    public ResponseEntity<List<Review>> getAllReviews(){
+        return new ResponseEntity<>(reviewRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/reviews/{id}")
+    public ResponseEntity<Optional<Review>> getReviewById(@PathVariable Long id){
         return new ResponseEntity<>(reviewRepository.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping(value = "/reviews")
     public ResponseEntity createReview(@RequestBody Review review){
         reviewRepository.save(review);
         return new ResponseEntity<>(review, HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/reviews/{id}")
     public ResponseEntity<Optional<Review>> deleteReview(@PathVariable Long id){
         reviewRepository.deleteById(id);
         return new ResponseEntity(reviewRepository.findAll(), HttpStatus.OK);
